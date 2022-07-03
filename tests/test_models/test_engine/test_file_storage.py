d2488a3feb9bc2f779138a52ec7c9a0df3cfda9c
Canel420 <4521@holbertonschool.com>
2022-07-03 12:05:48 -0500

    test for reload storage function

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -5,9 +5,12 @@ Unittest session for file_storage.py
 """
 
 import unittest
+import os
+from models import base_model
 from models.engine import file_storage
 
 FileStorage = file_storage.FileStorage
+BaseModel = base_model.BaseModel
 
 
 class TestStorage(unittest.TestCase):
@@ -15,5 +18,22 @@ class TestStorage(unittest.TestCase):
     def setUp(self):
         """ Sets up FileStorage object for each test """
         self.storage = FileStorage()
+        self.base = BaseModel()
 
     def test_instance(self):
+        """ Test instance creation """
+        self.assertEqual(type(self.storage), FileStorage)
+
+    def test_reload(self):
+        """ Test storage reload function """
+        self.base.name = "Manu"
+        self.base.save()
+        base_id = self.base.id
+        flag = 0
+        self.storage.reload()
+        objects = self.storage.all()
+        for key in objects.keys():
+            if base_id in key:
+                flag += 1
+        self.assertTrue(1 == flag)
+        os.remove('./file.json')
