b8a848be31de3419afb136e726f1707e270ecd5e
Santiago Bedoya <santiagobedoa@gmail.com>
2022-07-03 18:48:47 -0500

    New tests added to FileStorage test cases

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -26,6 +26,7 @@ class TestStorage(unittest.TestCase):
 
     def test_reload(self):
         """ Test storage reload function """
+        # self.base = BaseModel()
         self.base.name = "Manu"
         self.base.save()
         base_id = self.base.id
@@ -37,3 +38,34 @@ class TestStorage(unittest.TestCase):
                 flag += 1
         self.assertTrue(1 == flag)
         os.remove('./file.json')
+
+    def test_save_file(self):
+        """ Test storage save the file """
+        # self.base = BaseModel()
+        self.base.save()
+        self.assertTrue(os.path.exists('./file.json') == True)
+        os.remove('./file.json')
+
+    def test_save_content(self):
+        """ Test storage save the correct content """
+        # self.base = BaseModel()
+        self.base.name = "Santi"
+        base_id = self.base.id
+        self.base.save()
+        flags = 0
+        with open('./file.json', 'r') as file:
+            read_file = file.read()
+            if "Santi" in read_file:
+                flags += 1
+            if base_id in read_file:
+                flags += 1
+        self.assertTrue(flags == 2)
+        os.remove('./file.json')
+
+    def test_all(self):
+        """ Test storage all function """
+        # self.base = BaseModel()
+        dict_objs = dict()
+        obj = f"{type(self.base).__name__}.{self.base.id}"
+        dict_objs[obj] = self.base
+        self.assertEqual(dict_objs, self.storage.all())
