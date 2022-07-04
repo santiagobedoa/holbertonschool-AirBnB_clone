9693782e5ec14bd12e141be547a3938adefaeafb
Santiago Bedoya <santiagobedoa@gmail.com>
2022-07-04 16:03:39 -0500

    User class created

---
+++ models/user.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" User class """
+
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """
+    class that represents a user
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
