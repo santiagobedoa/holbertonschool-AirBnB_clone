91c210f1149fd3071c325d6b6d7bb433b72f7c51
Santiago Bedoya <santiagobedoa@gmail.com>
2022-07-04 11:22:01 -0500

    console: show method implemented

--- console.py
+++ console.py
@@ -3,6 +3,7 @@
 
 import cmd
 from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -19,6 +20,25 @@ class HBNBCommand(cmd.Cmd):
             print(new.id)
         else:
             print("** class doesn't exist **")
+
+    def do_show(self, line):
+        """prints the str representation of an instance"""
+        tokens = line.split()
+        if len(tokens) == 0:
+            print("** class name missing **")
+        else:
+            if tokens[0] == "BaseModel":
+                if len(tokens) == 1:
+                    print("** instance id missing **")
+                else:
+                    objects = storage.all()
+                    for key in objects.keys():
+                        if str(tokens[1]) in key:
+                            print(objects[key])
+                        else:
+                            print("** no instance found **")
+            else:
+                print("** class doesn't exist **")
 
     def do_EOF(self, line):
         """ Exit the interpreter cleanly """
