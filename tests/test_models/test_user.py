21c71d8290220aef85809157a1658129668b4093
Canel420 <4521@holbertonschool.com>
2022-07-05 17:00:25 -0500

    fixed user test

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -39,18 +39,30 @@ class TestUser(unittest.TestCase):
     def test_email_attr(self):
         """ Test if email is an attribute of user """
         self.assertTrue(hasattr(self.user, 'email'))
+        self.assertEqual(self.user.email, "")
+        self.user.email = "holbie@holbertonschool.com"
+        self.assertEqual(self.user.email, "holbie@holbertonschool.com")
 
     def test_password_attr(self):
         """ Test if password is an attribute of user """
         self.assertTrue(hasattr(self.user, 'password'))
+        self.assertEqual(self.user.password, "")
+        self.user.password = "*#$@&"
+        self.assertEqual(self.user.password, "*#$@&")
 
     def test_first_attr(self):
         """ Test if first_name is an attribute of user """
         self.assertTrue(hasattr(self.user, 'first_name'))
+        self.assertEqual(self.user.first_name, "")
+        self.user.first_name = "holbie"
+        self.assertEqual(self.user.first_name, "holbie")
 
     def test_last_attr(self):
         """ Test if last_name is an attribute of user """
         self.assertTrue(hasattr(self.user, 'last_name'))
+        self.assertEqual(self.user.last_name, "")
+        self.user.last_name = "school"
+        self.assertEqual(self.user.last_name, "school")
 
 
 if __name__ == "__main__":
